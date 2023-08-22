5b0bf2d9be4364238628ceb4b05dfc870d1f7040
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2023-08-22 14:10:50 -0300

    Aula 3
    Importando fontes, criando variáveis e criando Rodapé

---
+++ alura-space/src/componentes/Rodape/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import facebook from '../../assets/facebook.svg'
+import instagram from '../../assets/instagram.svg'
+import twitter from '../../assets/twitter.svg'
+import styles from './Rodape.module.scss';
+
+export default function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <ul className={styles.rodape__lista}>
+                <li>
+                    <a href="https://www.facebook.com/AluraCursosOnline/" target="_blank" rel='noreferrer'>
+                        <img src={facebook} alt='Ícone'></img>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/aluraonline/?hl=en" target="_blank" rel='noreferrer'>
+                        <img src={instagram} alt='Ícone'></img>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/aluraonline?lang=en" target="_blank" rel='noreferrer'>
+                        <img src={twitter} alt='Ícone'></img>
+                    </a>
+                </li>
+            </ul>
+            <p>Desenvolvido por Alura.</p>
+        </footer>
+    )
+}
